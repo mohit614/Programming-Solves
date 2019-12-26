e3ea804886121727b27b344c5b61875c96aefc42
mohit614 <mail4frnd.mohit@gmail.com>
2019-12-26 10:44:52 +0530

    all permutations of a string with repeatitions

---
+++ 1.py
@@ -0,0 +1,9 @@
+from itertools import combinations_with_replacement
+from hashlib import md5
+import string
+from itertools import  product
+
+p=product("mohit",repeat=5)
+
+for i in p:
+        print  "".join(i)
